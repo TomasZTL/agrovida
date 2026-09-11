6bdfd0c3243a6bbc83b25f3a6f10989784b79c3f
TomasZTL <tomas.zamorano06@inacapmail.cl>
2026-09-11 17:50:00 -0300

    Commit 3: configuración de urls.py con la ruta hacia la vista

--- agrovida_sistema/urls.py
+++ agrovida_sistema/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from catalogo.views import mostrar_productos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mostrar_productos, name='mostrar_productos'),
 ]
